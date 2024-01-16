f8da4c15d2bd9c1c693474c16e6aa159af7d7643
Mahvish162 <mahvishfatima162@gmail.com>
2024-01-16 09:24:49 +0530

    till now we are done with Adding loading spinner & variable pageSize to NewsMonkey

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
     render() {
+        
         let {title,description,imgurl,newsUrl}=this.props;
         return (
+        <>
       <div>
             <div className="card" style={{width: "18rem"}}>
-                <img src={imgurl} className="card-img-top" alt="..." />
+                <img src={imgurl==null?"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d0d211578d8c4562ab652371985d96e9":imgurl} className="card-img-top" alt="..." />
                     <div className="card-body">
                            <h5 className="card-title">{title}</h5>
                            <p className="card-text">{description}</p>
-                           <a href="/" className="btn btn-sm btn-primary">News</a>
+                           <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More...</a>
                     </div>
            </div>
-      </div>
+      </div>      
+      </>
     )
   }
 }
